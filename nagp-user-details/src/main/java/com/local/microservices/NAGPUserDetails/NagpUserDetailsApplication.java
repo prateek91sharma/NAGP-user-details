5b266116a831fabc4f6548fc7a20962929a891cf
Prateek Sharma <prateek.sharma02@nagarro.com>
2020-07-09 13:09:53 +0530

    Initial Commit

---
+++ nagp-user-details/src/main/java/com/local/microservices/NAGPUserDetails/NagpUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPUserDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpUserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagpUserDetailsApplication.class, args);
+	}
+
+}
---
+++ nagp-user-details/src/test/java/com/local/microservices/NAGPUserDetails/NagpUserDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.microservices.NAGPUserDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpUserDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
